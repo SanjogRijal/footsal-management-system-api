1341bf000166ca0a1f65035d958da17bf60e896e
sanjogR <sanjogrijal15@gmail.com>
2024-09-09 02:59:19 +0545

    feat: players read all service

--- src/modules/players/service/players/players.service.ts
+++ src/modules/players/service/players/players.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PlayersEntity } from '../../entities/index.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  constructor(
+    @InjectRepository(PlayersEntity)
+    private readonly playersRepository: Repository<PlayersEntity>,
+  ) {}
+
+  getAllPlayers() {
+    return this.playersRepository.find();
+  }
+}
